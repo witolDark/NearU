c7670a1831a5be830f1422cbc93fc2726a830af7
witolDark <witalikspelina@gmail.com>
2024-11-26 13:41:39 +0200

    Details page done Fixes

--- src/app/main/event-card/event-card.component.ts
+++ src/app/main/event-card/event-card.component.ts
@@ -10,5 +10,23 @@ import {Status} from '../../shared/enums/Status';
 export class EventCardComponent {
   @Input() event: EventPayload | undefined;
 
+  getStatusColor(status: Status | null | undefined) {
+    if(status) {
+      switch (status) {
+        case Status.ACTIVE:
+          return 'green';
+        case Status.INACTIVE:
+          return 'red';
+        case Status.PENDING:
+          return 'gray';
+        case Status.UPCOMING:
+          return 'blue';
+        default:
+          return 'black';
+      }
+    }
+    return 'black';
+  }
+
   protected readonly Status = Status;
 }
